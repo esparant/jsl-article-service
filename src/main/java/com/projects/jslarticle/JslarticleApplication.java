4265c162fe0ec981e549d1643b3ebdf3cf16ba7e
esparant <esparant2@gmail.com>
2025-08-18 20:10:58 +0900

    프로젝트 초기 설정 및 기초 파일 생성
    1. resources 내부에 있는 application.properties 를 .yml 확장자로 변경했습니다.
    2. test 용 application.yml 파일을 수정해 테스트 환경에서 프로젝트를 설정할 수 있도록 파일을 추가했습니다.

---
+++ src/main/java/com/projects/jslarticle/JslarticleApplication.java
@@ -0,0 +1,13 @@
+package com.projects.jslarticle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JslarticleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JslarticleApplication.class, args);
+	}
+
+}
