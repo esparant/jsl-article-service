d3b8ad3882afbfe3b1b09e164cc0ef7c076a4f73
mj2sdev <mj2sdev@gmail.com>
2025-08-22 22:29:26 +0900

    front 브랜치 기초공사

---
+++ src/main/java/com/projects/jslarticle/TemplateGlobalViewerController.java
@@ -0,0 +1,47 @@
+package com.projects.jslarticle;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * TemplateGlobalViewerController
+ * 모든 HTML Template 파일을 단일 엔드포인트로 서빙하는 컨트롤러
+ * 
+ * @author MJ2
+ * @since 2025.08.22
+ * 
+ * @note
+ * - 개발용/프론트 전용
+ * - favicon 요청 무시
+ * - TODO: 실제 컨트롤러 연결 시점부터 해당 컨트롤러 삭제 해야함 -> [이 컨트롤러가 다른 요청보다 먼저 가로채서 방해할 수 있기 때문]
+ * @tip todo: << 이거 넣으면 경고쪽에 알릴수 있음 ^^
+ * 
+ * @usage
+ * 	GET /index -> "templates/index.html" 뷰 반환
+ *  GET /good/morning -> "templates/good/morning.html" 뷰 반환
+ */
+@Controller
+@RequestMapping("/")
+public class TemplateGlobalViewerController {
+
+	/**
+	 * favicon 요청 무시용
+	 */
+	@GetMapping("favicon.ico")
+	@ResponseBody
+	String favicon(){ return ""; }
+	
+	/**
+	 * 모든 페이지 보기용 함수
+	 * (프론트전용)
+	 * @param filename
+	 * @return
+	 */
+	@GetMapping("{filename}")
+	String show(@PathVariable("filename") String filename) {
+		return filename;
+	}
+}
