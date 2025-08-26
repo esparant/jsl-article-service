d0be72b626008ffd44a85df44fde542cb193c453
esparant <esparant2@gmail.com>
2025-08-26 20:15:30 +0900

    Spring Security 에 관한 config 클래스 파일을 생성했습니다.
    1. SpringSecurityConfig - Spring Security 클래스 파일입니다.
    2. SecurityFilterChgain Bean - 요청설정과, 커스텀 로그인, 로그아웃을 설정했습니다.
    3. UserDetailService - Spring Security Login 테스트를 위한 데이터입니다.
    4. PasswordEncoder - 기본 PasswordEncoder 를 설정하는 Bean 입니다.

---
+++ src/main/java/com/projects/jslarticle/springsecurity/SpringSecurityConfig.java
@@ -0,0 +1,73 @@
+package com.projects.jslarticle.springsecurity;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+/**
+ * @author 탁영복
+ * @version 1.0
+ * @description spring security 에 관한 기본적인 설정입니다.
+ * @since 2025-08-26
+ */
+@Configuration
+@EnableWebSecurity
+public class SpringSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        /*
+         * @authorizeHttpRequests - 모든 요청을 허용한다.(임시)
+         * @formLogin - spring security 의 기본 로그인을 이용하지 않고 커스텀한 로그인 창을 이용한다.
+         * @logout - spring security 의 기본 로그아웃 기능을 이용하지 않고 커스텀해서 사용한다.
+         */
+        http.authorizeHttpRequests(auth -> auth
+                        .anyRequest().authenticated()
+                )
+                .formLogin(form -> form
+                        .loginPage("/login")
+                        .defaultSuccessUrl("/")
+                        .permitAll()
+                )
+                .logout(logout -> logout
+                        .logoutUrl("/logout")
+                        .logoutSuccessUrl("/"));
+
+        return http.build();
+    }
+
+    /*
+     * 테스트용 로그인 데이터 입니다.
+     */
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
+        UserDetails user = User.withUsername("user")
+                .password(passwordEncoder.encode("1234"))
+                .roles("USER")
+                .build();
+
+        UserDetails admin = User.withUsername("admin")
+                .password(passwordEncoder.encode("1234"))
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+
+    /*
+     * 기본 PasswordEncoder 을 BCrypt 로 설정합니다.
+     */
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
