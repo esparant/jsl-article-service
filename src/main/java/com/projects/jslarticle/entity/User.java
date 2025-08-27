175426ffcfbe046daa64b9831592b58a6e6590b0
esparant <esparant2@gmail.com>
2025-08-27 19:18:58 +0900

    User Entity 생성
    1. User Entity 를 생성했습니다. 추후 엔티티가 추가 되고 나서 수정될 예정입니다.:

--- src/main/java/com/projects/jslarticle/entity/User.java
+++ src/main/java/com/projects/jslarticle/entity/User.java
@@ -1,4 +1,49 @@
 package com.projects.jslarticle.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+/**
+ * @author 탁영복
+ * @since 2025-08-27
+ * @version 1.0
+ * @description User 엔티티입니다. 추가 엔티티 제작후 수정필요 합니다.
+ */
+@Entity
+@Getter
+@EntityListeners(AuditingEntityListener.class)
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String email;
+
+    private String password;
+
+    private String nickname;
+
+    private String tag;
+
+    private Long point;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+    private String profileImageUrl;
+
+    private boolean isDeleted;
+
 }
