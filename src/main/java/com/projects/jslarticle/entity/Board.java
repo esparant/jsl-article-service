7fe8010021b7dc0eb46cfa0dce9a01a8ca210d35
esparant <esparant2@gmail.com>
2025-08-27 19:26:09 +0900

    Board Entity 생성

---
+++ src/main/java/com/projects/jslarticle/entity/Board.java
@@ -0,0 +1,40 @@
+package com.projects.jslarticle.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+/**
+ * @author 탁영복
+ * @since 2025-08-27
+ * @version 1.0
+ * @description Board 엔티티입니다. 추가 엔티티 제작후 수정필요 합니다.
+ */
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+public class Board {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String boardName;
+
+    private String boardDescription;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+    private boolean isDeleted;
+}
