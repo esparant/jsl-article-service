43490159417f3ffbf6f0a5478db952fef9398131
esparant <esparant2@gmail.com>
2025-08-28 15:39:58 +0900

    ArticeCategory Entity 를 생성했습니다.

---
+++ src/main/java/com/projects/jslarticle/entity/ArticleCategory.java
@@ -0,0 +1,49 @@
+package com.projects.jslarticle.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+/**
+ * @author 탁영복
+ * @since 2025-08-27
+ * @version 1.0
+ * @description Article Category 엔티티입니다. 추가 엔티티 제작후 수정필요 합니다.
+ */
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+public class ArticleCategory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "board_id")
+    private Board board;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "role_id")
+    private Role role;
+
+    private String categoryName;
+
+    private int sortOrder;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+}
