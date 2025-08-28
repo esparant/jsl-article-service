575f538ef86583346649175fb7e25e6ced86d84f
esparant <esparant2@gmail.com>
2025-08-28 16:11:05 +0900

    Article Entity 생성

---
+++ src/main/java/com/projects/jslarticle/entity/Article.java
@@ -0,0 +1,57 @@
+package com.projects.jslarticle.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+/**
+ * @author 탁영복
+ * @since 2025-08-27
+ * @version 1.0
+ * @description Article Category 엔티티입니다. 추가 엔티티 제작후 수정필요 합니다.
+ */
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+public class Article {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "article_category_id")
+    private ArticleCategory articleCategory;
+
+    private boolean isNotice;
+
+    private String title;
+
+    private String content;
+
+    private String imageUrl;
+
+    private int views;
+
+    private int likeCount;
+
+    private int dislikeCount;
+
+    private boolean isDeleted;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+}
