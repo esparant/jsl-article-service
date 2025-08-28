990f9b9edc6c1b013e06989e7b4e823fc47b1856
esparant <esparant2@gmail.com>
2025-08-28 16:11:23 +0900

    Article Entity 생성

--- src/main/java/com/projects/jslarticle/entity/Article.java
+++ src/main/java/com/projects/jslarticle/entity/Article.java
@@ -18,7 +18,7 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
  * @author 탁영복
  * @since 2025-08-27
  * @version 1.0
- * @description Article Category 엔티티입니다. 추가 엔티티 제작후 수정필요 합니다.
+ * @description Article 엔티티입니다. 추가 엔티티 제작후 수정필요 합니다.
  */
 @Entity
 @EntityListeners(AuditingEntityListener.class)
