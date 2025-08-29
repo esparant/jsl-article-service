862ec777f1b41ec5c9a1948e090f1c6880827e2d
esparant <esparant2@gmail.com>
2025-08-29 15:32:26 +0900

    Admin Entity 작성

---
+++ src/main/java/com/projects/jslarticle/entity/Admin.java
@@ -0,0 +1,59 @@
+package com.projects.jslarticle.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.ForeignKey;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import lombok.Getter;
+
+/**
+ * @author 탁영복
+ * @version 1.0.0
+ * @description Admin Entity 입니다. 추가 Entity 제작후 수정필요 합니다.
+ * @since 2025-08-29
+ */
+@Table(name = "admin")
+@Entity
+@Getter
+public class Admin {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(
+            name = "board_id",
+            foreignKey = @ForeignKey(name = "fk_admin_board_id")
+    )
+    private Board board;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(
+            name = "user_id",
+            nullable = false,
+            foreignKey = @ForeignKey(name = "fk_admin_user_id")
+    )
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(
+            name = "role_id",
+            nullable = false,
+            foreignKey = @ForeignKey(name = "fk_admin_role_id")
+    )
+    private Role role;
+
+    @Column(updatable = false, nullable = false)
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
+    private LocalDateTime deletedAt;
+}
