7ae48371db0a4b97f67402ac3d526fce15a3f9f6
esparant <esparant2@gmail.com>
2025-08-29 14:24:34 +0900

    Comment Entity 에 Content Entity 상속

--- src/main/java/com/projects/jslarticle/entity/Comment.java
+++ src/main/java/com/projects/jslarticle/entity/Comment.java
@@ -1,31 +1,22 @@
 package com.projects.jslarticle.entity;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
-import java.time.LocalDateTime;
 import java.util.List;
 import lombok.Getter;
 
 /**
  * @author 탁영복
- * @version 1.0
+ * @version 1.0.0
  * @description Comment Entity 입니다. 추가 Entity 제작후 수정필요 합니다.
  * @since 2025-08-28
  */
 @Entity
 @Getter
-public class Comment {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+public class Comment extends Content {
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "article_id")
@@ -38,14 +29,5 @@ public class Comment {
     @OneToMany(mappedBy = "parent")
     private List<Comment> children;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id")
-    private User user;
-
     private String content;
-
-    @Column(updatable = false)
-    private LocalDateTime createdAt;
-
-    private LocalDateTime updatedAt;
 }
