abff6065ccc4a4a9ffc778606b50876904ff9cb7
esparant <esparant2@gmail.com>
2025-08-29 14:56:08 +0900

    ArticleLike Entity @Column, @Table, 제약 조건 이름 추가

--- src/main/java/com/projects/jslarticle/entity/ArticleLike.java
+++ src/main/java/com/projects/jslarticle/entity/ArticleLike.java
@@ -3,11 +3,13 @@ package com.projects.jslarticle.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
+import jakarta.persistence.ForeignKey;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import java.time.LocalDateTime;
 import lombok.Getter;
 
@@ -17,6 +19,7 @@ import lombok.Getter;
  * @description ArticleLike Entity 입니다. 추가 Entity 제작후 수정필요 합니다.
  * @since 2025-08-28
  */
+@Table(name = "article_like")
 @Entity
 @Getter
 public class ArticleLike {
@@ -26,15 +29,24 @@ public class ArticleLike {
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "article_id")
+    @JoinColumn(
+            name = "article_id",
+            nullable = false,
+            foreignKey = @ForeignKey(name = "fk_article_like_article_id")
+    )
     private Article article;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id")
+    @JoinColumn(
+            name = "user_id",
+            nullable = false,
+            foreignKey = @ForeignKey(name = "fk_article_like_user_id")
+    )
     private User user;
 
     @Column(updatable = false)
     private LocalDateTime createdAt;
 
+    @Column(updatable = false)
     private Boolean isBad;
 }
