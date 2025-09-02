59fa5ab804b7c5c77c02a1a28d59e84c09f23552
esparant <esparant2@gmail.com>
2025-09-02 15:33:50 +0900

    Bookmark Entity UK 제약조건추가

--- src/main/java/com/projects/jslarticle/entity/Bookmark.java
+++ src/main/java/com/projects/jslarticle/entity/Bookmark.java
@@ -10,6 +10,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 import java.time.LocalDateTime;
 import lombok.Getter;
 
@@ -19,7 +20,12 @@ import lombok.Getter;
  * @description Bookmark Entity 입니다. 추가 Entity 제작후 수정필요 합니다.
  * @since 2025-08-29
  */
-@Table(name = "bookmark")
+@Table(
+        name = "bookmark",
+        uniqueConstraints = {
+                @UniqueConstraint(name = "uk_bookmark_user_id_article_id", columnNames = {"user_id", "artilce_id"})
+        }
+)
 @Entity
 @Getter
 public class Bookmark {
